8787770d601ae442b895ca2e437ca28111187a86
ztyhalo <ztyswa@163.com>
2018-08-13 14:17:48 +0800

    初次提交版本，能够实现基本功能。

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,9 +10,7 @@
 QRect  GetScreenInfo(void)
 {
    QDesktopWidget* desktopWidget = QApplication::desktop();
-   //获取可用桌面大小
 //       QRect deskRect = desktopWidget->availableGeometry();
-   //获取设备屏幕大小
    QRect screenRect = desktopWidget->screenGeometry();
 
    return screenRect;
